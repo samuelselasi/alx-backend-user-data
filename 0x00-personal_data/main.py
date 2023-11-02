c8d779c7b7deada8c58f56042fde6c1ecf076853
samuelselasi <samelsekasi@gmail.com>
2023-11-02 15:46:10 +0000

    Add main file to test task 6

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,7 +4,14 @@ Main file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
+
+bob@dylan:~$
+bob@dylan:~$ ./main.py
+b'$2b$12$Fnjf6ew.oPZtVksngJjh1.vYCnxRjPm2yt18kw6AuprMRpmhJVxJO'
+True

532ce597ec10fab8290add8298b5ed6ed40a6af0
samuelselasi <samelsekasi@gmail.com>
2023-11-02 15:37:35 +0000

    Add function to encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Function returns salted and hashed password taken in string format"""
+
+    encrypted = password.encode()
+    hashed_password = bcrypt.hashpw(encrypted, bcrypt.gensalt())
+    return hashed_password

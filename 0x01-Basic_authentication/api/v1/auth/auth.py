a8c34eb2844304033f7f16c30b2c76510dc3d54b
samuelselasi <samelsekasi@gmail.com>
2023-11-06 20:57:12 +0000

    Add module that handles authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that manages API authentication"""
+from flask import request
+from typing import List
+
+
+class Auth():
+    """Class that manages API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method that manages authentication"""
+
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Funtion that manages authorization header"""
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user """
+
+        return None

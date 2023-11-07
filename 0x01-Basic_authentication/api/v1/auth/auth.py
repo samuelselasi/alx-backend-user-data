6cee4a6fa5b0d393e2c4b40e80885c8b219fabe9
samuelselasi <samelsekasi@gmail.com>
2023-11-07 10:36:50 +0000

    Update authorization_header to handle request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,13 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """Funtion that manages authorization header"""
 
-        return None
+        if request is None:
+            return None
+
+        if not request.headers.get("Authorization"):
+            return None
+
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """

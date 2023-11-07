9d97419ca6c00b5232ee021b4bc71a7b8de1d755
samuelselasi <samelsekasi@gmail.com>
2023-11-07 12:13:18 +0000

    Add empty class that inherits from Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that manages API authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class that manages basic API authentication"""
+
+    pass

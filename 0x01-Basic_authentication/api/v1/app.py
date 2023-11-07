4d2fede50093ae5d154df0ffb4f13886864fcac6
samuelselasi <samelsekasi@gmail.com>
2023-11-07 12:01:49 +0000

    Modify api/v1/app.py to use BasicAuth class instead of Auth depending on the value of environment variable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -15,8 +17,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(401)

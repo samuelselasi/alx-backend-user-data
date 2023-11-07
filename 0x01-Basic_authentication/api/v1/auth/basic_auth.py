f1887de1c7a03572ab3ce359305840bddb6ae580
samuelselasi <samelsekasi@gmail.com>
2023-11-07 12:36:41 +0000

    Add method that retirns the Base64 part of the Authorization header for a basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class that manages basic API authentication"""
 
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Method that  returns Base64 part of Authorization header"""
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        base = authorization_header.split(' ')
+        return base[1]

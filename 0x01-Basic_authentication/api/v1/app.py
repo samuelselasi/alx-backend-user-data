e2943c354de4076003abf4a82c24b459e8b6381f
samuelselasi <samelsekasi@gmail.com>
2023-11-07 10:30:16 +0000

    Add method to hanle before_request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -35,6 +35,26 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.before_request
+def before_request() -> str:
+    """Method to handler before request"""
+
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")

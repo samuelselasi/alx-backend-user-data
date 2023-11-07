5fdd9b1e0a94aec12e2f06cdff2bb1a09fbc95a9
samuelselasi <samelsekasi@gmail.com>
2023-11-07 10:00:10 +0000

    Update require_auth method to return true if path is not in the list of strings in excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,21 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method that manages authentication"""
 
-        return False
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+
+        for i in excluded_paths:
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
+                    return False
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Funtion that manages authorization header"""

b322081acb390cfd1a988954adb466e94289a5c1
samuelselasi <samelsekasi@gmail.com>
2023-11-09 10:43:29 +0000

    Add an empty class to handle session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Session authentication """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class that defines instances of session authentication"""
+
+    pass

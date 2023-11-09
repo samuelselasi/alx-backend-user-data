f98cf5f799ddefd725f4f529894397e2c07def02
samuelselasi <samelsekasi@gmail.com>
2023-11-09 19:52:38 +0000

    Update session imports

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -26,6 +27,8 @@ elif AUTH_TYPE == "session_auth":
     auth = SessionAuth()
 elif getenv('AUTH_TYPE') == 'session_exp_auth':
     auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(401)

fc3f23d45b6f2beef1ecc3303fb33a9a8f5a5d31
samuelselasi <samelsekasi@gmail.com>
2023-11-09 10:48:35 +0000

    Update api/v1/app.py for using SessionAuth instance for the variable auth depending of the value of the environment variable AUTH_TYPE

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ if AUTH_TYPE == 'auth':
     auth = Auth()
 elif AUTH_TYPE == 'basic_auth':
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)

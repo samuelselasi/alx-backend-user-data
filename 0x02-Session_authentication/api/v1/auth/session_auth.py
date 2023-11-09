a4a619a5e39a7d7d019c553b57357440fa8fbb67
samuelselasi <samelsekasi@gmail.com>
2023-11-09 17:33:59 +0000

    Add method to delete a user session or logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -44,3 +44,22 @@ class SessionAuth(Auth):
 
         user_id = self.user_id_for_session_id(session_cookie)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Method to delete the user session / logout"""
+
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True

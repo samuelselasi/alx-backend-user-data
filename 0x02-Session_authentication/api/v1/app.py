1df617d6fcfd8d37eeaef593b5a76e38fdb46762
samuelselasi <samelsekasi@gmail.com>
2023-11-09 18:30:39 +0000

    Update app.py t instantiate auth with SessionExpAuth if the environment variable AUTH_TYPE is equal to session_exp_auth.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -23,6 +24,8 @@ elif AUTH_TYPE == 'basic_auth':
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
     auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(401)

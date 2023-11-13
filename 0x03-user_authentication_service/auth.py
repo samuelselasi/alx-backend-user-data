25b8f2eb9961654f3e8b0df868dbbe0064c6bb10
samuelselasi <samelsekasi@gmail.com>
2023-11-13 19:28:23 +0000

    Add method that returns User object based on email and password string arguments

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 """Module handling user authentication"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> str:
     """Function to encodes password in string to bytes"""
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method that returns a User object based on email and password"""
+
+        try:
+            user = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            userPwd = _hash_password(password)
+            user = self._db.add_user(email, userPwd)
+
+            return user
+
+        else:
+            raise ValueError('User {email} already exists')

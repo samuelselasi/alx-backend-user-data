92e5d250c6b1d2e8846204b2139e2db399a8be5e
samuelselasi <samelsekasi@gmail.com>
2023-11-13 19:39:15 +0000

    Add endpoint that returns a JSON payload

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module containing flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """Endpoint that returns a JSON payload"""
+
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

5c31dd4f6cf9c4c6065e71f3e63ee9597b13d370
samuelselasi <samelsekasi@gmail.com>
2023-11-14 23:08:09 +0000

    Fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,7 +53,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
-    if user is None:
+    if session_id is None:
         abort(403)
 
     AUTH.destroy_session(user.id)

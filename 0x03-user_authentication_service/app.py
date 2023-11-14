8490d81e11e36633154ef6fc5d2062e525e53816
samuelselasi <samelsekasi@gmail.com>
2023-11-14 12:37:34 +0000

    Add endpoint to login with email and password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module containing flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -27,6 +27,23 @@ def users():
 
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Endpoint to login a user with email and password"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    res = jsonify({'email': email, 'message': 'logged in'})
+
+    res.set_cookie('session_id', session_id)
+    return res
 
 
 if __name__ == "__main__":

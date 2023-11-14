0ec819d79284c8ce3efccdd468d07f5595b5ed12
samuelselasi <samelsekasi@gmail.com>
2023-11-14 23:27:44 +0000

    Fix logout endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,8 +52,8 @@ def logout():
 
     session_id = request.cookies.get('session_id')
 
-    # if session_id is None:
-    # abort(403)
+    if session_id is None:
+        abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
 

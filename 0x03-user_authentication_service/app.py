b4b4879b67f7bcefeca6bb8ee3a8b34870570768
samuelselasi <samelsekasi@gmail.com>
2023-11-15 17:11:13 +0000

    Add endpoint to update password with email and token from form data

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,5 +95,22 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """Endpoint that updates password with email and token from form data"""
+
+    email = request.form.get('email')
+    token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(token, password)
+
+    except Exception as err:
+        return jsonify({"error": str(err)}), 403
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
